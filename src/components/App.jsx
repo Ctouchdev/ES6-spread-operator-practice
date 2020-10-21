18b27295f9b08f228f23d202e8bbdcffa4d39994
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-21 14:16:15 -0500

    Updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [listItem, setListItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleInput(e) {
+    const itemValue = e.target.value;
+
+    setListItem(itemValue);
+  }
+
+  function addItem() {
+    setListItem("");
+    setItems((prevItems) => {
+      return [...prevItems, listItem];
+    });
+  }
+
+  console.log(listItem);
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          onChange={handleInput}
+          type="text"
+          name="listItems"
+          value={listItem}
+          placeholder="Enter Item"
+        />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => {
+            return <li>{todoItem}</li>;
+          })}
         </ul>
       </div>
     </div>
